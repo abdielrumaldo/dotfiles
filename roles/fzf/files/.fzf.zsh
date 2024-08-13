af754d17dfba36a3434cc2087014906e2c0f1c15
Abdiel Rumaldo <abdielrumaldom@gmail.com>
2024-08-13 16:04:16 -0500

    configuring fzf the right way

--- roles/fzf/files/.fzf.zsh
+++ roles/fzf/files/.fzf.zsh
@@ -1,13 +1,26 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/abdielrumaldo/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/abdielrumaldo/bin"
+
+if [[ ! "$PATH" == *${FZF_PATH}/bin* ]]; then
+  export PATH="$PATH:${FZF_PATH}/bin"
 fi
 
 # Auto-completion
 # ---------------
-source /opt/homebrew/opt/fzf/shell/completion.zsh
+[[ $- == *i* ]] && source "${FZF_PATH}/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
+source "${FZF_PATH}/shell/key-bindings.zsh"
+
+# if [[ ! "$PATH" == */Users/abdielrumaldo/bin* ]]; then
+#   PATH="${PATH:+${PATH}:}/Users/abdielrumaldo/bin"
+# fi
+#
+# # Auto-completion
+# # ---------------
+# source /opt/homebrew/opt/fzf/shell/completion.zsh
+#
+# # Key bindings
+# # ------------
+# source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
